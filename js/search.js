36d6963a27a2daaf3df9140acf764de1c3f18b1c
Javier Cejudo <javier@javiercejudo.com>
2012-07-11 02:06:42 +0200

    load suggestions on tab

--- js/search.js
+++ js/search.js
@@ -8,23 +8,24 @@ window.addEvent('domready', function() {
 		},
 		onComplete: function(response) {
 			timeout = setTimeout(function () {
-				//if (response !== '') {
-			        $('news-container').set('html', response);
-				//}
+			    $('news-container').set('html', response);
 				$('news-container').setStyle('opacity', '1');
 			}, 100);
 		}
 	});
 	
 	$('q').addEvent('input', function(event){
-		if(event) { event.preventDefault(); }
-		var q = $('q').get('value');
-		req.get({ q : q });
+		req.get({ 'q' : $('q').get('value') });
 	});
 	
 	$$('.outer-container').addEvent('click:relay(a.top_suggestion_link)', function(event, target){
 		if(event) { event.preventDefault(); }
 		$('q').set('value', target.get('html'));
 		$('q').fireEvent('input');
+		$('q').focus();
+	});
+	
+	$$('.outer-container').addEvent('focus:relay(a.top_suggestion_link)', function(event, target){
+		$$('.outer-container').fireEvent('click:relay(a.top_suggestion_link)', [event, target]);
 	});
 });
