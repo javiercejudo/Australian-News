b5fad7eb27b35d76718640dd081f67c2d1887c0f
Javier Cejudo <javier@javiercejudo.com>
2012-07-14 18:35:40 +0200

    small fix

--- js/search.js
+++ js/search.js
@@ -32,7 +32,7 @@ window.addEvent('domready', function() {
 				auxElement.inject($('more-items-container'), 'before');
 				var num_top = parseInt($('num').get('value'));
 				var num_tot = parseInt($('num_total').get('html'));
-				if (num_top > num_tot) {
+				if (num_top >= num_tot) {
 					var num_sho = num_tot;
 					$('more-items-container').dispose();
 				} else {
