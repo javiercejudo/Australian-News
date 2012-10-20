fde4bd0b89bca07ea03bad46f286d4f49df05445
Javier Cejudo <javier@javiercejudo.com>
2012-10-20 12:05:22 +0200

    methods not static anymore except load_XML

--- lib/App.php
+++ lib/App.php
@@ -4,6 +4,35 @@ require_once DIRLIB . 'DB.php';
 
 class App {
 
+	function __construct(&$num, &$skip, &$q) {
+		$num  = self::set_num();
+		$skip = self::set_skip();
+		$q    = self::set_q();
+	}
+	
+	private function set_num() {
+		if (isset($_GET['ns'])) {
+			return DURATION;
+		}
+		if (isset($_GET['num']) && !empty($_GET['num']) && intval($_GET['num']) > 0) {
+			return $_GET['num'];
+		}
+		return DURATION;
+	}
+	
+	private function set_skip() {
+		if (isset($_GET['skip']) && !empty($_GET['skip']) && intval($_GET['skip']) > 0) {
+			return $_GET['skip'];
+		}
+		return 0;
+	}
+	
+	private function set_q() {
+		if (isset($_GET['q']) && !empty($_GET['q'])) {
+			return trim($_GET['q']);
+		}
+	}
+
 	static public function load_XML() {
 		$local_url = LOCAL_DATA;
 		if (is_file($local_url) && date('U') - filemtime($local_url) < 60*10-10) {
@@ -15,27 +44,4 @@ class App {
 			DB::do_insert($xml);
 		}
 	}
-	
-	static public function set_num() {
-		if (isset($_GET['ns'])) {
-			return DURATION;
-		}
-		if (isset($_GET['num']) && !empty($_GET['num']) && intval($_GET['num']) > 0) {
-			return $_GET['num'];
-		}
-		return DURATION;
-	}
-	
-	static public function set_skip() {
-		if (isset($_GET['skip']) && !empty($_GET['skip']) && intval($_GET['skip']) > 0) {
-			return $_GET['skip'];
-		}
-		return 0;
-	}
-	
-	static public function set_q() {
-		if (isset($_GET['q']) && !empty($_GET['q'])) {
-			return trim($_GET['q']);
-		}
-	}	
 }
